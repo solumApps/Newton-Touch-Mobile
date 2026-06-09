5bb5df5055f60ae0520601adcf26dbf920419fc2
kumarichanda-solu-m <149353167+kumarichanda-solu-m@users.noreply.github.com>
2026-06-09 15:01:39 +0530

    Remove .angular cache from tracking

--- .angular/cache/20.3.26/app/vite/deps/p-BmVRXR1y-YTKZTMUC.js
+++
@@ -1,7 +0,0 @@
-import {
-  e
-} from "./chunk-4SKD3W26.js";
-import "./chunk-WDMUDEB6.js";
-export {
-  e as startFocusVisible
-};
--- .angular/cache/20.3.26/app/vite/deps/p-Cl0B-RWe-H6NRW7E2.js
+++
@@ -1,9 +0,0 @@
-import {
-  h,
-  s
-} from "./chunk-Q7PV5YUG.js";
-import "./chunk-WDMUDEB6.js";
-export {
-  h as GESTURE_CONTROLLER,
-  s as createGesture
-};
